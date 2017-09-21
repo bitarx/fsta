1612d32873492ddfa560b40b2b60fb7481795127
bitarx <imanishi@bitarx.jp>
2017-09-21 09:58:40 +0900

    フリスタとはを作成

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -12,14 +12,42 @@
 <div id="wrapper">
     <div id="content">
         <section>
-            <article id="post-51" class="content">
+            <article id="desc" class="content">
                 <header>
                     <h2 class="title"><span>ふりスタとは？</span></h2>
                 </header>
-                <div class="post toppage">
-                    <p><img src="/images/post51.png" alt="" width="230" class="alignleft" /><br>
-「 Free　Style　Study　System 」（ フリー・スタイル・スタディ・システム ）の略称で、「 自由な形式の学習形態 」を目的としています。<br><br>
-                    </p>
+                <div class="post">
+                    「 Free　Style　Study　System 」（ フリー・スタイル・スタディ・システム ）の略称で、「 自由な形式の学習形態 」を目的としています。
+                    <h3>１.『 Free　Style 』（学習指導形態）</h3>
+                    <p><img src="images/banners/desc1.png" alt="" width="231" class="alignleft">
+                        A.「 スタンダード・少人数プラン 」〔 少人数（1クラス6～8名程度）一斉指導 〕<br>
+B.「 エクストラ・個別指導プラン 」〔 1対1、完全個別指導 〕<br>
+C.「 ゲスト・模試生プラン 」<br>〔 模擬試験のみ希望する場合 〕
+・『 ふりスタ 』では『 新みやぎ模試 』が受験可能です。
+<br><br><br><br><br><br><br><br></p>
+                    <h3>２.『 Free　Time 』（ 学習時間の選択 ）</h3>
+                    <p><img src="images/banners/desc2.png" alt="" width="231" class="alignright">B.「 エクストラ・個別指導プラン 」の場合、（ 担当講師と相談の上 ）学習指導時間の変更や延長が可能です。<br>また、生徒の体調不良の場合など指導時間を短くすることがあります（ 指導料金は30分単位の精算になります ）
+<br><br><br><br><br><br><br><br><br></p>
+                    <h3>３.『 Free　Study 』（ 自習可能 ）</h3>
+                    <p><img src="images/banners/desc1.png" alt="" width="231" class="alignleft">塾生（ 模試生を除く）は、自習室として教室を自由にお使いいただけます。自習可能な時間帯を教室でご確認ください。
+<br><br><br><br><br><br><br><br><br><br><br><br></p>
+                    <h3>４.『 Free　Drinks 』（ 水分補給サービス ）</h3>
+                    <p><img src="images/banners/desc2.png" alt="" width="231" class="alignright">模擬試験の実施日に限り、『ふりスタCafe』が無料となります。模擬試験の受験者は、ご自由にジュース類をお飲みください。
+<br><br><br><br><br><br><br><br><br><br><br><br></p>
+                    <h3>５.『 振り返り　Study 』（ 選択可能な指導内容 ）</h3>
+                    <p><img src="images/banners/desc1.png" alt="" width="231" class="alignleft"><br>B.「 エクストラ・個別指導プラン 」の場合、模試対策、定期試験対策、検定試験対策、歴史や化学反応式の学習し直しなど、教室長が指導方針を決め、学習担当講師と生徒が相談してその日の学習内容を決めます。一方的にその日の指導内容を決めることはありません。<br><br><br><br><br><br><br><br></p>
+                    <h3>６.『 Free　Selection 』（ 自由な問題選択 ）</h3>
+                    <p><img src="images/banners/desc2.png" alt="" width="231" class="alignright">A.「 スタンダード・少人数プラン 」の場合、その日のテーマを講師とともに確認します。<br>
+その後、BASIC基礎確認、反復TRAINING、実戦CHALLENGE、から問題を選択します。<br>
+・BASIC基礎確認……　学校の授業内容レベルの問題となります。BASIC（ ベーシック、基礎 ）の確認を行います。<br>
+・反復TRAINING………　50点の得点または偏差値50突破を目指します。TRAINING（ トレーニング ）を積んで、とにかく沢山の問題を解いて頂きます。<br>
+・実践CHALLENGE…　80点以上の突破を目指します。難問・奇問、何でも挑戦します。<br><br>
+
+※   B.「 エクストラ・個別指導プラン」においても、上記の問題選択は可能です。担当講師もしくは、教室長にご相談ください。
+<br><br></p>
+                    <h3>７. 『 Free　Star 』を一緒に目指しましょう</h3>
+                    <p><img src="images/banners/desc1.png" alt="" width="231" class="alignleft">生徒ひとり一人の性格や適性は異なるものです。一斉集団指導の場においてすら、その学習指導方法にこだわるのが、本当の意味での「 個別指導 」であります。<br>
+他の人よりも頑張って、他の人よりも問題を1問でも多く解いて、「 立派な自由な星 」のように輝きましょう。</p>
                 </div>
             </article>
         </section>
