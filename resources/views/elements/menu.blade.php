d3ff99196be5abf97822482b1c46b2d2f0f0ca53
bitarx <imanishi@bitarx.jp>
2017-09-21 10:52:41 +0900

    フリスタとは画像作成

--- resources/views/elements/menu.blade.php
+++ resources/views/elements/menu.blade.php
@@ -1,14 +1,14 @@
             <ul>
                 @if (controllerName() == 'ContactController')
                     <li><a href="/#"><strong>Top</strong></a></li>
-                    <li><a href="/#post-51"><strong>ふりスタとは</strong></a></li>
+                    <li><a href="/#desc"><strong>ふりスタとは</strong></a></li>
                     <li><a href="/#post-52"><strong>指導料金</strong></a></li>
                     <li><a href="/#post-53"><strong>合格実績</strong></a></li>
                     <li><a href="/#post-54"><strong>アクセス</strong></a></li>
                     <li><a href="/contact"><strong>お問い合わせ</strong></a></li>
                 @else
                     <li><a href="#"><strong>Top</strong></a></li>
-                    <li><a href="#post-51"><strong>ふりスタとは</strong></a></li>
+                    <li><a href="#desc"><strong>ふりスタとは</strong></a></li>
                     <li><a href="#post-52"><strong>指導料金</strong></a></li>
                     <li><a href="#post-53"><strong>合格実績</strong></a></li>
                     <li><a href="#post-54"><strong>アクセス</strong></a></li>
