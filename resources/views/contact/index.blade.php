3921ac4af175a9bc586773dc3db64cc69fb5849a
bitarx <imanishi@bitarx.jp>
2017-09-25 23:09:14 +0900

    問い合わせのcss修正

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -16,7 +16,7 @@
                 <dl>
                     <dt>お名前<br></dt>
                     <dd><input type="text" id="name"></dd>
-                    <dt>メールアドレス<br>(必須)</dt>
+                    <dt>メールアドレス(必須)</dt>
                     <dd><input type="text" id="mail"></dd>
                     <dt>自由記述欄</dt>
                     <dd><textarea name="" id="message" cols="30" rows="10"></textarea></dd>
