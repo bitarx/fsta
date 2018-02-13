0aa23f47d041e15dd1fce46f0b16f73875b4dfb7
imanishi@bitarx.jp <imanishi@bitarx.jp>
2018-02-13 22:57:22 +0900

    指導形式メニュー修正

--- resources/views/elements/menu.blade.php
+++ resources/views/elements/menu.blade.php
@@ -11,7 +11,7 @@
                     <li><a href="#"><strong>Top</strong></a></li>
                     <li><a href="#desc"><strong>ふりスタとは</strong></a></li>
                     <li><a href="#fee"><strong>指導料金</strong></a></li>
-                    <li><a href="/#format"><strong>指導形式</strong></a></li>
+                    <li><a href="#format"><strong>指導形式</strong></a></li>
                     <li><a href="#case"><strong>合格実績</strong></a></li>
                     <li><a href="#access"><strong>アクセス</strong></a></li>
                     <li><a href="/contact"><strong>お問い合わせ</strong></a></li>
