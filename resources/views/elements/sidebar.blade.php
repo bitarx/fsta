1ab5b05007630aea850093e245b045342b507cc1
bitarx <imanishi@bitarx.jp>
2018-02-14 13:01:30 +0900

    ライン強調追加

--- resources/views/elements/sidebar.blade.php
+++ resources/views/elements/sidebar.blade.php
@@ -4,7 +4,7 @@
                 <h3 class="title"><span>お知らせ</span></h3>
             </div>
             <div class="post news">
-                <p><time datetime="2018-2-13">2017/2/13</time><span>サイト情報更新しました！</span>
+                <p><time datetime="2018-2-13">2018/2/13</time><span>サイト情報更新しました！</span>
                 <p><time datetime="2017-11-16">2017/11/16</time><span>サイト情報更新しました！</span>
                 <p><time datetime="2017-9-30">2017/9/30</time><span>サイトオープンしました！</span>
             </div>
--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -18,18 +18,23 @@
                     <h2 class="title"><span>ふりスタとは？</span></h2>
                 </header>
                 <div class="post">
-                    <p>「 Free　Style　Study　System 」（ フリー・スタイル・スタディ・システム ）の略称で、「 自由な形式の学習形態 」を目的としています。</p>
+                    <p><span class="mark">「 Free　Style　Study　System 」（ フリー・スタイル・スタディ・システム ）</span>の略称で、「 <span class="mark">自由な形式の学習形態</span> 」を目的としています。</p>
                     <h3>☆『 Free　Style 』（学習指導形態）</h3>
                     <p><img src="images/banners/desc1.png" alt="" width="201" class="alignleft">
                         <span class="strong">A. 「 家庭教師・個別指導プラン 」〔 1対1・担任制完全個別スタイル 〕</span><br>
-                        ・その日の学習内容において、生徒さんから特にリクエストがない場合、教室長の準備した教材で学習を進めます。模試対策、定期試験対策、検定試験対策など、教室長が指導方針を決め、学習担当講師と生徒さんが相談してその日の学習内容を決めます。一方的にその日の指導内容を決めることはありません。<br>
+                        ・その日の学習内容において、生徒さんから特にリクエストがない場合、教室長の準備した教材で学習を進めます。<br>
+                        　模試対策、定期試験対策、検定試験対策など、教室長が指導方針を決め、学習担当講師と生徒さんが相談してその日の学習内容を決めます。<br>
+                        　一方的にその日の指導内容を決めることはありません。<br>
                         ・国語や英語の「 長文の読み合わせ 」などにも、じっくり時間を使えます。<br><br>
 
                         <span class="strong">B. 「スタンダード・個別指導プラン」〔中学生対象：少人数・個別指導スタイル（ 1クラス3名程度）〕</span><br>
-                        ・生徒さん一人ひとりの「 短期目標 」に従い、教室長が指示する内容を学習します。その為、生徒さん一人ひとりの学習内容も変わります。「 定期試験対策 」や「 模擬試験対策 」、「 振り返り学習 」などを行います。<br><br>
+                        ・生徒さん一人ひとりの「 短期目標 」に従い、教室長が指示する内容を学習します。<br>
+                        　その為、生徒さん一人ひとりの学習内容も変わります。<br>
+                        　「 定期試験対策 」や「 模擬試験対策 」、「 振り返り学習 」などを行います。<br><br>
 
                         <span class="strong">C. 「 特訓講習スタンダードプラン 」〔 中学生対象：少人数クラス指導 〕【 季節講習期間中や定期試験前の限定開講 】</span><br>
-                        ・問題演習に特化したコースになります。教室長の用意した問題の演習中心の学習になります。<br><br>
+                        ・問題演習に特化したコースになります。<br>
+                        　教室長の用意した問題の演習中心の学習になります。<br><br>
 
                         <span class="strong">D．「 ゲスト模擬試験受験生プラン 」〔 模擬試験の受験のみを希望する場合 〕</span><br>
                         ・『 ふりスタ 』では『 新みやぎ模試 』が受験可能です（ 受験後の成績表は郵送になります ）。
@@ -37,7 +42,7 @@
 
                     <h3>☆『 Free　Study 』 教室の使用は自由です〔 A、B、Cプラン対象 〕</h3>
                     <p><img src="images/banners/desc2.png" alt="" width="201" class="alignleft">
-                       塾生（ 模試生を除く ）は、自習室として教室を自由にお使いいただけます。<br>「 時と精神の自習ブース 」で静かに勉強することができます。<br>自習時間の質と量が合格のカギを握ります！<br><br>
+                       塾生（ 模試生を除く ）は、自習室として教室を自由にお使いいただけます。<br>「 <span class="mark">時と精神の自習ブース</span> 」で静かに勉強することができます。<br>自習時間の質と量が合格のカギを握ります！<br><br>
                         自習室の使用例<br>
                         ① <span class="notice">土・日・祝日や季節講習期間中など</span><br>
                         ９：00～12：00　スタンダード個別指導クラス<br>
@@ -46,18 +51,18 @@
                         16：30～19：00　自習室として教室を開放<br><br></p>
 
                     <h3>☆ 一人ひとりのオーダーメイドプログラム〔 A、Bプラン対象 〕</h3>
-                    <p><img src="images/banners/classroom.png" alt="" width="231" class="alignright">子供たち一人ひとりの性格や学力は異なるものです。<br>一人ひとりのオーダーメイド学習プログラムなので、「 わかりきった問題の繰り返し 」や「 難しすぎて一人では解けない 」と言った、無駄な時間がありません。時間を有効に使うことができます。</p>
+                    <p><img src="images/banners/classroom.png" alt="" width="231" class="alignright">子供たち一人ひとりの性格や学力は異なるものです。<br>一人ひとりのオーダーメイド学習プログラムなので、<span class="mark">「 わかりきった問題の繰り返し 」や「 難しすぎて一人では解けない 」と言った、無駄な時間がありません。</span><br>時間を有効に使うことができます。</p>
 
                     <h3>☆ 『 長期目標の設定 』＆『 短期目標の修正 』</h3>
                     <p><img src="images/banners/mendan_boy.png" alt="" width="231" class="alignleft">
-                        入塾面談時に「 年間長期目標 」をお伺いします。<br>定期試験や模擬試験の度に「 短期目標 」の見直しを行います。「 短期目標 」とは「 次の試験までの学習内容の指導計画 」になります。<br><br><br><br><br><br></p>
+                        入塾面談時に「 年間長期目標 」をお伺いします。<br>定期試験や模擬試験の度に「 短期目標 」の見直しを行います。<br><span class="mark">「 短期目標 」とは「 次の試験までの学習内容の指導計画 」</span>になります。<br><br><br><br></p>
 
                     <h3>☆ 『 Free　Star 』を一緒に目指しましょう</h3>
-                    <p><img src="images/banners/success.png" alt="" width="231" class="alignleft">他の人よりも頑張って、他の人よりも問題を1問でも多く解いて、「 立派な自由な星 」のように輝きましょう。<br><br><br><br><br><br></p>
+                    <p><img src="images/banners/success.png" alt="" width="231" class="alignleft">他の人よりも頑張って、他の人よりも問題を1問でも多く解いて、「 <span class="mark">立派な自由な星</span> 」のように輝きましょう。<br>受験だけではなく、世の中は競争だらけです。なんでも頑張って、自分の限界を広げましょう。<br><br><br></p>
 
 
                     <h3>☆ 『 Free　Drinks 』（ 水分補給サービス ）</h3>
-                    <p><img src="images/banners/jihan.png" alt="" width="190" class="alignright">模擬試験の実施日に限り、『 ふりスタCafé 』が無料となります。模擬試験の受験者は、ご自由にジュース類をお飲みください。<br><br><br>
+                    <p><img src="images/banners/jihan.png" alt="" width="190" class="alignright"><span class="mark">模擬試験の実施日に限り、『 ふりスタCafé 』が無料</span>となります。<br>模擬試験の受験者は、ご自由にジュース類をお飲みください。<br><br>
                     <span class="notice">新みやぎ模試・実施予定日</span>
                     <table class="statsDay">
                         <tr><td>３月25日(日)</td><td><span class="strong">【 新小５ ～ 新中３生対象 】</span></td></tr>
@@ -164,9 +169,10 @@
                      ( 小学生 選択3教科12カ月分、2,000円)( 国語・算数・英語など )<br>
                     ※ 教材を持参する場合（ 学校のワーク類や他塾で使っていた教材を使用したい場合など ）、年間教材費は不要です。<br>
                     （4） ご家庭での指導を希望する場合、別途派遣費用が必要になります。<br>
-                    （5） 退塾・併塾・休塾は自由です。お引止めはしませんが、塾に復帰する場合、一定の条件を加えさせて頂く場合があります。<br>
-                    （6） 学習指導の時間帯と料金は飽くまでも目安となります。時間を延長して授業を受けた場合や、時間を短縮して授業を受けた場合は30分単位の清算になります。<br>
-                    （7） 季節や時間帯によって（ 講師のスケジュールの都合上 ）、指導日時が塾優先で相談させていただく場合があります。<br>
+                     （5） 併塾の問題はありませんが、学習指導内容の重複を避けるため、他塾さんでの学習指導の進捗状況をお伺いさせて頂きます。<br>
+                    （6） 退塾・休塾は自由です。お引止めはしませんが、塾に復帰する場合、一定の条件を加えさせて頂く場合があります。<br>
+                    （7） 学習指導の時間帯と料金は飽くまでも目安となります。時間を延長して授業を受けた場合や、時間を短縮して授業を受けた場合は30分単位の清算になります。<br>
+                    （8） 季節や時間帯によって（講師のスケジュールの都合上）、指導日時が塾優先で相談させていただく場合があります。<br>
                     </p>
                 </div>
             </article>
@@ -184,16 +190,16 @@
                     <h3>中学生 ＆ 高校受験生</h3>
                     <p>
                         ・〔 TOP高 狙い撃ち 〕 ナンバースクール ＆ 国立高専狙い撃ち<br>
-中学3年生への進級を待たずに、中学生範囲の数学や英語の学習を終える生徒さんもいます。（ ご希望の場合、高校生範囲の数学や英語も指導します。 ）<br>
+　中学3年生への進級を待たずに、中学生範囲の数学や英語の学習を終える生徒さんもいます。<br>　<span class="mark">ご希望の場合、高校生範囲の数学や英語も指導します。</span><br>
 ・〔 定期試験 ＆ 実力試験対策 〕 総合学力の充実<br>
-「 定期試験の対策 」と「 模擬試験の対策 」が学習指導の中心になります。<br>
+　<span class="mark">「 定期試験の対策 」と「 模擬試験の対策 」が学習指導の中心</span>になります。<br>
 ・〔 ベーシック ＆ 定期試験 〕　基礎学力の充実<br>
-「 既習範囲の確認 及び 基礎学力の定着 」と「 定期試験の対策 」が学習指導の中心になります。学習習慣の定着が目標になります。
+　<span class="mark">「 既習範囲の確認 及び 基礎学力の定着 」と「 定期試験の対策 」が学習指導の中心</span>になります。<br>　学習習慣の定着が目標になります。
                     </p>
                     <h3>小学生</h3>
                     <p>
                         ・〔 予習 + 復習 〕 学習習慣の定着<br>
-学校の授業内容の予習と復習が中心になります。算数(数学)や英語の先取り学習や、各種検定試験対策も行います。いっしょに『 We can 』の学習も行えます。
+　<span class="mark">学校の授業内容の予習と復習が中心</span>になります。<br>　<span class="mark">算数(数学)や英語の先取り学習や、各種検定試験対策</span>も行います。<br>　いっしょに『 We can 』の学習も行えます。
                     </p>
 <br>
 <p>『 ふりスタ 』は「 <span class="strong">新しくて、自由な学習スタイル</span> 」の確立を目指しております。</p>
@@ -210,55 +216,56 @@
 『 <b>教室長プロフィール</b> 』<br><br>
 <span class="strongMessage">宮城教育大学卒業後、集団指導学習塾に勤務。<br>
 スキルアップの為、個別指導学習塾（M）の教室長に就任。<br>
-スキルアップの為、大手家庭教師塾（K）に約10年間勤務。そのほとんどの期間を宮城県No.1 家庭教師として実績を残しました。満を持しての独立開業です。<br>
+スキルアップの為、大手家庭教師塾（K）に約10年間勤務。<br>そのほとんどの期間を宮城県No.1 家庭教師として実績を残しました。<br>満を持しての独立開業です。<br>
 あらゆる塾業界を経験した教室長が選択した学習指導スタイルは『 超・個別指導 』です。
 </span></p><br>
 
 <img src="/images/banners/daigaku.png" alt="" width="230" class="alignleft" />
 
 <p>以下は過去の合格実績の一部です。</p>
-<p>☆   <span class="notice">I・K くん</span>（Ｈ29）<span class="strong">東北福祉大学・総合福祉学部・社会福祉学科</span>（ＡＯ入試２期）<br>
-（Ｈ26）<span class="highschool">柴田高校・体育学科</span>（前期選抜）【 大河原中学校出身 】</p>
+<p>☆   <span class="notice">I・K くん</span>（Ｈ29）<span class="strong">東北福祉大学・総合福祉学部・社会福祉学科</span>（ＡＯ２期）<br>
+（Ｈ26）<span class="highschool">柴田高校・体育学科</span>（前期）【 大河原中 】</p>
 
-<p>☆   <span class="notice">H・M さん</span>（Ｈ29）<span class="highschool">柴田高校・普通科</span>（前期選抜試験）【 福岡中 】</p>
+<p>☆   <span class="notice">H・M さん</span>（Ｈ29）<span class="highschool">大河原商業・流通マネジメント科</span>（前期選抜試験）【 福岡中 】</p>
 
-<p>☆   <span class="notice">H・O さん</span>（Ｈ29）<span class="highschool">大河原商業・流通マネジメント科</span>（前期選抜試験）【白石東中】</p>
+<p>☆   <span class="notice">H・O さん</span>（Ｈ29）<span class="highschool">柴田高校・普通科</span>（前期選抜試験）【白石東中】</p>
 
 <p>☆   <span class="notice">R・O くん</span>（Ｈ28）<span class="strong">仙台高専・電気科専攻科</span>（推薦合格）≪4年生まで指導≫<br>
-　　　　　　（Ｈ24）<span class="strong">仙台高専・電気システム学科</span>（一般受験）【 角田中学校出身 】</p>
+　　　　　　（Ｈ24）<span class="strong">仙台高専・電気システム学科</span>（一般）【 角田中 】</p>
 
 <p>☆   <span class="notice">Y・H くん</span>（H27）<span class="strong">東北大学法学部</span>（センター試験及び２次試験での数学指導）<br>
 　　　　　　（H24）<span class="highschool">仙台三高合格</span>（後期選抜）</p>
 
 <p>☆   <span class="notice">I・S くん</span>（Ｈ28）<span class="strong">東北福祉大学健康科学部リハビリテーション学科作業療法学専攻</span>
-(校内推薦合格)<br>
-　　　　　　（Ｈ25）<span class="highschool">泉松陵高校・普通科</span>（後期選抜試験）【 金ケ瀬中学校出身 】</p>
+(校内推薦)<br>
+　　　　　　（Ｈ25）<span class="highschool">泉松陵高校・普通科</span>（後期選抜）【 金ケ瀬中 】</p>
 
 <p>☆   <span class="notice">Y・H くん</span>（Ｈ27）<span class="strong">東北大学法学部（センター試験及び２次試験での数学指導）</span><br>
-　　　　　　（Ｈ24）<span class="highschool">仙台三高合格</span>（後期選抜）</p>
+　　　　　　（Ｈ24）<span class="highschool">仙台三高合格</span>（後期）</p>
 
-<p>☆   <span class="notice">M・S さん</span>（Ｈ27）<span class="strong">福島大学・人文社会科学群・人間発達文化学類</span>（推薦合格）<br>
-　　　　　　（Ｈ24）<span class="highschool">白石高校・普通科合格</span>【 白石東中学校出身 】</p>
+<p>☆   <span class="notice">M・S さん</span>（Ｈ27）<span class="strong">福島大学・人文社会科学群・人間発達文化学類</span>（推薦）<br>
+　　　　　　（Ｈ24）<span class="highschool">白石高校・普通科合格</span>(後期)【 白石東中学校出身 】</p>
 
 <br>
-<p>☆   【 H28 】　<span class="strong">新潟薬科大学・薬学部</span>【白石高】、 <span class="strong">新潟薬科大学・薬学部</span>仙台高専・建築デザイン学科</span>（推薦合格）、<span class="highschool">柴田高校</span>（普通科）、<span class="strong">盛岡大学</span>（文学部）、<span class="strong">仙台高専・建築デザイン学科</span>（推薦合格）【遠刈田中学校出身】</p>
-<p>☆   【 H27 】　<span class="highschool">名取北高</span>（後期選抜）、 <span class="highschool">宮城第一高等学校</span>（後期選抜）【角田中出身】</p>
-<p>☆   【 H26 】　<span class="highschool">白石高校・看護学科</span>（後期選抜）【白石東中学校出身】、<span class="strong">仙台高専・マテリアル環境</span>（推薦合格）【金津中学校出身】</p>
+<p>☆   【 H28 】　<span class="strong">新潟薬科大学・薬学部</span>【白石高】、<span class="strong">盛岡大学（文学部）</span>【白石高】、<span class="strong">仙台高専・建築デザイン学科</span>（推薦）【遠刈田中】、<span class="highschool">柴田高校・普通科</span>（後期）【福岡中】</p>
 
-<p>☆   【 H25 】　<span class="strong">山形大学医学部看護学科</span>（センター試験での数学指導【白石高】、<span class="highschool">尚絅学院中学、村田高校、角田高校、白石工業</span>（機械科）、<span class="highschool">白石工業</span>（電気科）</p>
+<p>☆   【 H27 】　<span class="highschool">名取北高</span>（後期）、 <span class="highschool">宮城第一高等学校</span>（後期）【角田中】</p>
+<p>☆   【 H26 】　<span class="highschool">白石高校・看護学科</span>（後期）【白石東中】、<span class="strong">仙台高専・マテリアル環境</span>（推薦）【金津中】</p>
 
-<p>【 その他 】　<span class="strong">東北文化学園大学、工学院大学</span>、<span class="highschool">白石工業高等学校</span>（２名、電気科、機械科）、<span class="strong">宮城教育大学中等教育教員養成課程後期合格</span>（数学指導）、<span class="strong">仙台大学</span>（校内推薦）、<span class="highschool">角田高校 、白石高校、村田高校</span>（推薦合格）、<span class="highschool">村田高校</span>（一般受験）、<span class="highschool">名取高校</span></p>
+<p>☆   【 H25 】　<span class="strong">山形大学医学部看護学科</span>（センター試験での数学指導）【白石高】、<span class="highschool">尚絅学院中学</span>【船岡中】、<span class="highschool">村田高校</span>【宮中】、<span class="highschool">角田高校</span>【川崎中】、<span class="highschool">白石工業
+（機械科）</span>【遠刈田中】、<span class="highschool">白石工業（電気科）</span>【大張中】、（Ｈ25）<span class="strong">東北福祉大学・総合マネジメント学部</span>（一般）【角田高】</p>
+
+<p>【 その他 】　<span class="strong">東北文化学園大学、工学院大学</span>、<span class="highschool">白石工業高等学校</span>（２名、電気科、機械科）、<span class="strong">宮城教育大学中等教育教員養成課程後期合格</span>（数学指導）、<span class="strong">仙台大学</span>（校内推薦）、<span class="highschool">角田高校 、白石高校、村田高校</span>（推薦）、<span class="highschool">村田高校</span>（一般）、<span class="highschool">名取高校</span></p>
 <br>
 <br>
 <p>≪　特に多い進学先　≫</p>
-<p>・<span class="strong">仙台高専</span>（３名うち２名が推薦合格）</p>
-<p>・<span class="highschool">白石高校</span>（4名うち1名が看護学科）</p>
-<p>・<span class="highschool">柴田高校</span>（5名うち2名が前期合格）</p>
-<p>・<span class="highschool">村田高校</span>（3名うち1名が推薦合格）</p>
-<p>・<span class="highschool">角田高校</span>（3名）</p>
 <p>・<span class="strong">東北福祉大学</span>（3名）</p>
 <p>・<span class="strong">東北学院大学</span>（2名）</p>
-
+<p>・<span class="strong">仙台高専</span>（３名うち２名が推薦合格）</p>
+<p>・<span class="highschool">白石高校</span>（4名うち1名が看護学科）</p>
+<p>・<span class="highschool">角田高校</span>（3名）</p>
+<p>・<span class="highschool">柴田高校</span>（5名うち2名が前期合格、体育学科・前期１名）</p>
+<p>・<span class="highschool">村田高校</span>（3名うち1名が推薦合格）</p>
                 </div>
             </article>
         </section>
